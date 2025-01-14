6cff46163d9225f41cfd2f924e6e6cec24edaad6
Ihar Skavysh <igogoblin@gmail.com>
2025-01-14 21:18:20 +0300

    feat: add header base layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./index.pug";
 import "./styles/_const.scss";
 import "./styles/styles.scss";
+import "./components/mixins/mixins.scss";
 
-import "./components/header/header.module.scss";
+// import heads from "./components/header/header.module.scss";
 
 console.log("hotelScout is running!");
