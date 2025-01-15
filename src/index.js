6d98d802bdd4f50bcb44687116cd816b91fce88f
Ihar Skavysh <igogoblin@gmail.com>
2025-01-15 19:33:49 +0300

    refactor: footer position

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ import "./styles/styles.scss";
 import "./components/mixins/mixins.scss";
 import "./components/footer/footer.scss";
 
-// import heads from "./components/header/header.module.scss";
-
 console.log("hotelScout is running!");
