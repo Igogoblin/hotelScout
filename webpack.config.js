bb2940b1de0d92f402a397babe78379e841c03aa
Ihar Skavysh <igogoblin@gmail.com>
2025-01-20 21:58:34 +0300

    feat: add card calendar base layout mixin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WatchFilesPlugin = require("webpack-watch-files-plugin").default;
 
 module.exports = {
   mode: "development",
@@ -9,6 +10,7 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/hotelScout/",
+    clean: true,
     // publicPath: "/",
   },
   module: {
@@ -61,6 +63,13 @@ module.exports = {
       title: "hotelScout",
       filename: "index.html",
       template: "./src/index.pug",
+      cache: false,
+    }),
+    new WatchFilesPlugin({
+      files: ["./src/**/*.pug"],
+      options: {
+        force: true,
+      },
     }),
   ],
   devServer: {
@@ -68,16 +77,23 @@ module.exports = {
       directory: path.join(__dirname, "dist"),
       watch: true,
     },
-
+    hot: true,
     compress: true,
     port: 8080,
     open: true,
-    watchFiles: ["src/**/*"],
+    watchFiles: ["src/**/*.pug", "src/**/*.scss", "src/**/*.js"],
     liveReload: true,
     client: {
       overlay: true,
+      reconnect: true,
     },
+    watchFiles: ["src/**/*"],
   },
+  // watchOptions: {
+  //   aggregateTimeout: 200,
+  //   poll: 1000,
+  //   ignored: /node_modules/,
+  // },
   resolve: {
     extensions: [".js", ".pug", ".scss"],
   },
