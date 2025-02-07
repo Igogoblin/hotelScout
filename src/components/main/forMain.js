ac1f8bdbb5d563658c832261c706e937d7b3139f
Ihar Skavysh <igogoblin@gmail.com>
2025-02-07 10:57:28 +0300

    feat: add clear calendar for push buttons calendar

--- src/components/main/forMain.js
+++ src/components/main/forMain.js
@@ -1,3 +1,5 @@
+import AirDatepicker from "air-datepicker";
+
 // this is for main page. for initial data to search for room numbers
 const btnMinus = document.querySelectorAll(".btn--minus");
 const btnPlus = document.querySelectorAll(".btn--plus");
@@ -109,3 +111,91 @@ function checkGuests(isBabies = false) {
 
   return count;
 }
+
+// for calendar ============================================================================
+// mainCalendar
+document.addEventListener("DOMContentLoaded", function () {
+  const mainCalendar = document.getElementById("mainCalendar");
+  const mainArrival = document.querySelector(".main__arrival");
+  const mainDeparture = document.querySelector(".main__departure");
+  let calendarMain = document.querySelector(".air-datepicker");
+  // const mainArrivalDate = new Date(mainArrival.value);
+  // const mainDepartureDate = new Date(mainDeparture.value);
+  // mainCalendar.addEventListener("click", function () {
+  //   // mainCalendar.classList.has("none");
+  //   // (calendarMain.classList.contains("none")){
+
+  //   // }
+  //   console.log(calendarMain.classList.contains("showNone"));
+  // });
+  if (mainCalendar) {
+    mainCalendar.addEventListener("click", function () {
+      calendarMain = document.querySelector(".air-datepicker");
+
+      // Ensure AirDatepicker is visible before toggling
+      if (calendarMain) {
+        calendarMain.classList.remove("showNone");
+      } else {
+        console.warn("AirDatepicker element not found. Ensure it initializes.");
+      }
+    });
+  } else {
+    console.error("mainCalendar element not found in the DOM.");
+  }
+
+  new AirDatepicker(mainCalendar, {
+    range: true,
+    // multipleDatesSeparator: " - ",
+    inline: false,
+    classes: "showNone",
+    autoClose: false,
+    visible: false,
+    prevHtml: `<svg  class="arrow-icon" width="11" height="18"  fill="#BC9CFF" viewBox="0 0 11 18" xmlns="http://www.w3.org/2000/svg">
+    <path  class="arrow-path" d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/>
+    </svg>`,
+    nextHtml: `<svg width="13" height="11" viewBox="0 0 11 18" fill="#BC9CFF" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/>
+</svg>
+`,
+    navTitles: {
+      days: `<strong>MMMM</strong>${" "}<strong> yyyy</strong>`,
+    },
+    // selectedDates: [new Date()],
+    onSelect: (datepicker) => {
+      console.log("in onSelect ", datepicker);
+      console.log("datepicker all", datepicker.formattedDate);
+
+      if (datepicker.formattedDate[0]) {
+        let firstTime = datepicker.formattedDate[0].split(".");
+        mainArrival.value = `${firstTime[2]}-${firstTime[1]}-${firstTime[0]}`;
+      }
+      if (datepicker.formattedDate[1]) {
+        let secondTime = datepicker.formattedDate[1].split(".");
+        mainDeparture.value = `${secondTime[2]}-${secondTime[1]}-${secondTime[0]}`;
+      }
+    },
+    buttons: [
+      // "clear",
+      {
+        content: "Очистить", // Russian translation for "Clear"
+        onClick: (datepicker) => {
+          datepicker.clear(); // Example action: Clear selected date
+          clearCalendar();
+        },
+      },
+      {
+        content: "Применить", // Russian translation for "Apply"
+        onClick: (datepicker) => {
+          datepicker.hide(); // Example action: Close the picker on apply
+          clearCalendar();
+        },
+      },
+    ],
+  });
+  function clearCalendar() {
+    // calendarMain = document.querySelector(".air-datepicker");
+    setTimeout(() => {
+      calendarMain.classList.add("showNone");
+    }, 100);
+  }
+});
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./components/mixins/mixins.scss";
 import "./components/main/main.scss";
 import "./components/footer/footer.scss";
 import "./components/main/forMain.js";
-// import arrowBack from "@assets/arrow_back.svg";
+import arrowBack from "@assets/arrow_back.svg";
 
 // import flatpickr from "flatpickr";
 // import { Russian } from "flatpickr/dist/l10n/ru.js";
@@ -198,8 +198,15 @@ document.addEventListener("DOMContentLoaded", () => {
     range: true,
     multipleDatesSeparator: " - ",
     inline: true,
+    prevHtml: `<svg  class="arrow-icon" width="11" height="18"  fill="#BC9CFF" viewBox="0 0 11 18" xmlns="http://www.w3.org/2000/svg">
+    <path  class="arrow-path" d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/>
+    </svg>`,
+    nextHtml: `<svg width="13" height="11" viewBox="0 0 11 18" fill="#BC9CFF" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/>
+</svg>
+`,
     navTitles: {
-      days: `<strong>MMMM</strong>${" "}<strong> yyyy</strong>`,
+      days: `<strong>MMMM</strong><strong>yyyy</strong>`,
     },
     // selectedDates: [new Date()],
     onSelect: (datepicker) => {
