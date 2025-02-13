440d2ecd7f2fe7dff4fb28d75d855a5c368907db
Ihar Skavysh <igogoblin@gmail.com>
2025-02-13 12:04:17 +0300

    fix: change routing all in one another file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./components/main/main.scss";
 import "./components/header/header.pug";
 import "./components/footer/footer.pug";
 import "./components/footer/footer.scss";
+import "./routing/router.js";
 import "./components/main/forMain.js";
 import "./components/mixins/roomCard/roomCard.pug";
 import "./components/mixins/roomCard/roomCard.scss";
@@ -16,15 +17,15 @@ import "./components/mixins/slider/slider.js";
 import "./components/mixins/mburger/mburger.pug";
 import "./components/mixins/mburger/mburger.scss";
 import "./components/mixins/mburger/mburger.js";
-
-import { initializeCalendar } from "./components/main/forMain.js";
-import { mburger } from "./components/mixins/mburger/mburger.js";
+import "air-datepicker/air-datepicker.css";
+// import { initializeCalendar } from "./components/main/forMain.js";
+// import { mburger } from "./components/mixins/mburger/mburger.js";
 
 // import flatpickr from "flatpickr";
 // import { Russian } from "flatpickr/dist/l10n/ru.js";
 
-import AirDatepicker from "air-datepicker";
-import "air-datepicker/air-datepicker.css";
+// import AirDatepicker from "air-datepicker";
+
 // import "air-datepicker/air-datepicker.css";
 
 // import "./pages/about.pug";
@@ -53,73 +54,73 @@ import "air-datepicker/air-datepicker.css";
 //   loadContent(`.${path}`);
 // });
 
-function loadContent(url) {
-  fetch(url)
-    .then((response) => {
-      if (!response.ok)
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      return response.text();
-    })
-    .then((html) => {
-      document.body.innerHTML = html;
-      console.log("html ", url);
-      setTimeout(initializeScripts(url), 100);
-      // initializeScripts();
-      // console.log("html ", url);
-      // if (url === "./index.html") {
-      //   initializeCalendar();
-      // }
-    })
-    .catch((err) => {
-      document.body.innerHTML = "<h1>Error loading content</h1>";
-      console.error(err);
-    });
-}
+// function loadContent(url) {
+//   fetch(url)
+//     .then((response) => {
+//       if (!response.ok)
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       return response.text();
+//     })
+//     .then((html) => {
+//       document.body.innerHTML = html;
+//       console.log("html ", url);
+//       setTimeout(initializeScripts(url), 100);
+//       // initializeScripts();
+//       // console.log("html ", url);
+//       // if (url === "./index.html") {
+//       //   initializeCalendar();
+//       // }
+//     })
+//     .catch((err) => {
+//       document.body.innerHTML = "<h1>Error loading content</h1>";
+//       console.error(err);
+//     });
+// }
 
-function navigateTo(route) {
-  if (routes[route]) {
-    history.pushState({}, "", route);
-    loadContent(routes[route]);
-  } else {
-    console.log("Default route or 404 handler here.");
-  }
-}
+// function navigateTo(route) {
+//   if (routes[route]) {
+//     history.pushState({}, "", route);
+//     loadContent(routes[route]);
+//   } else {
+//     console.log("Default route or 404 handler here.");
+//   }
+// }
 
-const routes = {
-  "/": "./index.html",
-  "/hotelScout/": "./index.html",
-  "/hotelScout/about": "./about.html",
-  "/hotelScout/registration": "./registration.html",
-  "/hotelScout/enter": "./enter.html",
-  "/hotelScout/ui-colors": "./ui-colors.html",
-  "/hotelScout/ui-forms": "./forms.html",
-  "/hotelScout/ui-cards": "./cards.html",
-  "/hotelScout/ui-headers": "./headers.html",
-  "/hotelScout/room": "./room.html",
-  "/hotelScout/search": "./search.html",
-};
+// const routes = {
+//   "/": "./index.html",
+//   "/hotelScout/": "./index.html",
+//   "/hotelScout/about": "./about.html",
+//   "/hotelScout/registration": "./registration.html",
+//   "/hotelScout/enter": "./enter.html",
+//   "/hotelScout/ui-colors": "./ui-colors.html",
+//   "/hotelScout/ui-forms": "./forms.html",
+//   "/hotelScout/ui-cards": "./cards.html",
+//   "/hotelScout/ui-headers": "./headers.html",
+//   "/hotelScout/room": "./room.html",
+//   "/hotelScout/search": "./search.html",
+// };
 
-// Инициализация контента при загрузке
-const initialRoute = window.location.pathname;
-if (routes[initialRoute]) {
-  loadContent(routes[initialRoute]);
-} else {
-  console.log("Default route or 404 handler here.");
-}
+// // Инициализация контента при загрузке
+// const initialRoute = window.location.pathname;
+// if (routes[initialRoute]) {
+//   loadContent(routes[initialRoute]);
+// } else {
+//   console.log("Default route or 404 handler here.");
+// }
 
-// Обработка навигации без перезагрузки страницы
-document.addEventListener("click", (event) => {
-  const target = event.target.closest("a");
-  if (target && target.href.startsWith(window.location.origin)) {
-    event.preventDefault();
-    navigateTo(new URL(target.href).pathname);
-  }
-});
+// // Обработка навигации без перезагрузки страницы
+// document.addEventListener("click", (event) => {
+//   const target = event.target.closest("a");
+//   if (target && target.href.startsWith(window.location.origin)) {
+//     event.preventDefault();
+//     navigateTo(new URL(target.href).pathname);
+//   }
+// });
 
-// Обработка кнопок "Назад" и "Вперед"
-window.addEventListener("popstate", () => {
-  loadContent(routes[window.location.pathname]);
-});
+// // Обработка кнопок "Назад" и "Вперед"
+// window.addEventListener("popstate", () => {
+//   loadContent(routes[window.location.pathname]);
+// });
 // loadContent(routes[matchingRoute]);
 
 // document.querySelectorAll("a").forEach((link) => {
@@ -208,69 +209,60 @@ if (module.hot) {
 //   },
 
 // new AirDatepicker("myId", {});
-document.addEventListener("DOMContentLoaded", () => {
-  new AirDatepicker("#myId", {
-    range: true,
-    multipleDatesSeparator: " - ",
-    inline: true,
-    prevHtml: `<svg  class="arrow-icon" width="11" height="18"  fill="#BC9CFF" viewBox="0 0 11 18" xmlns="http://www.w3.org/2000/svg">
-    <path  class="arrow-path" d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/>
-    </svg>`,
-    nextHtml: `<svg width="13" height="11" viewBox="0 0 11 18" fill="#BC9CFF" xmlns="http://www.w3.org/2000/svg">
-<path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/>
-</svg>
-`,
-    navTitles: {
-      days: `<strong>MMMM</strong><strong>yyyy</strong>`,
-    },
-    // selectedDates: [new Date()],
-    onSelect: (datepicker) => {
-      console.log(datepicker.selectedDates);
-      console.log(datepicker);
-    },
-    buttons: [
-      // "clear",
-      {
-        content: "Очистить", // Russian translation for "Clear"
-        onClick: (datepicker) => {
-          datepicker.clear(); // Example action: Clear selected dates
-        },
-      },
-      {
-        content: "Применить", // Russian translation for "Apply"
-        onClick: (datepicker) => {
-          datepicker.hide(); // Example action: Close the picker on apply
-        },
-      },
-    ],
-  });
-  // new AirDatepicker("#my-element", {
-  //   buttons: ["очистить", "применить"],
-  //   multipleDatesSeparator: " - ",
-  //   range: true,
-  // });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   new AirDatepicker("#myId", {
+//     range: true,
+//     multipleDatesSeparator: " - ",
+//     inline: true,
+//     prevHtml: `<svg  class="arrow-icon" width="11" height="18"  fill="#BC9CFF" viewBox="0 0 11 18" xmlns="http://www.w3.org/2000/svg">
+//     <path  class="arrow-path" d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/>
+//     </svg>`,
+//     nextHtml: `<svg width="13" height="11" viewBox="0 0 11 18" fill="#BC9CFF" xmlns="http://www.w3.org/2000/svg">
+// <path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/>
+// </svg>
+// `,
+//     navTitles: {
+//       days: `<strong>MMMM</strong><strong>yyyy</strong>`,
+//     },
+//     // selectedDates: [new Date()],
+//     onSelect: (datepicker) => {
+//       console.log(datepicker.selectedDates);
+//       console.log(datepicker);
+//     },
+//     buttons: [
+//       // "clear",
+//       {
+//         content: "Очистить", // Russian translation for "Clear"
+//         onClick: (datepicker) => {
+//           datepicker.clear(); // Example action: Clear selected dates
+//         },
+//       },
+//       {
+//         content: "Применить", // Russian translation for "Apply"
+//         onClick: (datepicker) => {
+//           datepicker.hide(); // Example action: Close the picker on apply
+//         },
+//       },
+//     ],
+//   });
+//   // new AirDatepicker("#my-element", {
+//   //   buttons: ["очистить", "применить"],
+//   //   multipleDatesSeparator: " - ",
+//   //   range: true,
+//   // });
+// });
 
-function initializeScripts(url) {
-  const forPage = url.split("/")[1].split(".")[0];
-  console.log("forPage ", forPage);
-  switch (url) {
-    case "/about.html":
-      // initializeCalendar();
-      mburger();
-      break;
-    case "/index":
-    default:
-      initializeCalendar();
-      mburger();
-  }
-  // if (document.getElementById("mainCalendar")) {
-  //   initializeCalendar();
-  // } else {
-  //   console.warn("mainCalendar не найден, повторный вызов через 100ms");
-  //   setTimeout(initializeScripts, 100);
-  // }
-  // if (document.querySelector(".menu-btn")) {
-  //   mburger();
-  // }
-}
+// function initializeScripts(url) {
+//   const forPage = url.split("/")[1].split(".")[0];
+//   console.log("forPage ", forPage);
+//   switch (url) {
+//     case "/about.html":
+//       // initializeCalendar();
+//       mburger();
+//       break;
+//     case "/index":
+//     default:
+//       initializeCalendar();
+//       mburger();
+//   }
+// }
---
+++ src/routing/router.js
@@ -0,0 +1,84 @@
+import { initializeCalendar } from "../components/main/forMain.js";
+import { mburger } from "../components/mixins/mburger/mburger.js";
+function loadContent(url) {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      return response.text();
+    })
+    .then((html) => {
+      document.body.innerHTML = html;
+      console.log("html ", url);
+      setTimeout(() => initializeScripts(url), 100);
+      // initializeScripts();
+      // console.log("html ", url);
+      // if (url === "./index.html") {
+      //   initializeCalendar();
+      // }
+    })
+    .catch((err) => {
+      document.body.innerHTML = "<h1>Error loading content</h1>";
+      console.error(err);
+    });
+}
+
+function navigateTo(route) {
+  if (routes[route]) {
+    history.pushState({}, "", route);
+    loadContent(routes[route]);
+  } else {
+    console.log("Default route or 404 handler here.");
+  }
+}
+
+const routes = {
+  "/": "./index.html",
+  "/hotelScout/": "./index.html",
+  "/hotelScout/about": "./about.html",
+  "/hotelScout/registration": "./registration.html",
+  "/hotelScout/enter": "./enter.html",
+  "/hotelScout/ui-colors": "./ui-colors.html",
+  "/hotelScout/ui-forms": "./forms.html",
+  "/hotelScout/ui-cards": "./cards.html",
+  "/hotelScout/ui-headers": "./headers.html",
+  "/hotelScout/room": "./room.html",
+  "/hotelScout/search": "./search.html",
+};
+
+// Инициализация контента при загрузке
+const initialRoute = window.location.pathname;
+if (routes[initialRoute]) {
+  loadContent(routes[initialRoute]);
+} else {
+  console.log("Default route or 404 handler here.");
+}
+
+// Обработка навигации без перезагрузки страницы
+document.addEventListener("click", (event) => {
+  const target = event.target.closest("a");
+  if (target && target.href.startsWith(window.location.origin)) {
+    event.preventDefault();
+    navigateTo(new URL(target.href).pathname);
+  }
+});
+
+// Обработка кнопок "Назад" и "Вперед"
+window.addEventListener("popstate", () => {
+  loadContent(routes[window.location.pathname]);
+});
+
+function initializeScripts(url) {
+  const forPage = url.split("/").pop().split(".")[0];
+  console.log("forPage ", forPage);
+  switch (forPage) {
+    case "/about.html":
+      // initializeCalendar();
+      mburger();
+      break;
+    case "/index":
+    default:
+      initializeCalendar();
+      mburger();
+  }
+}
